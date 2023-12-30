1f3c409f9d7bdc879c6c43f159f38f7ef9da37ec
Dawid Drabczyk <dawid.drabczyk01@gmail.com>
2023-12-30 23:05:05 +0100

    Fix style, RWD and add ngStyle directive

--- src/app/station-list/station-item/station-item.component.ts
+++ src/app/station-list/station-item/station-item.component.ts
@@ -1,17 +1,18 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { StationDto } from '../station-dto.model';
-import { DatePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, NgStyle, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-station-item',
   standalone: true,
-  imports: [UpperCasePipe, DatePipe],
+  imports: [UpperCasePipe, DatePipe, NgStyle],
   templateUrl: './station-item.component.html',
   styleUrl: './station-item.component.scss',
 })
 export class StationItemComponent implements OnInit {
   public stationItem: StationDto | undefined;
+  public temperature?: number;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -20,9 +21,35 @@ export class StationItemComponent implements OnInit {
 
   ngOnInit(): void {
     this.stationItem = this.data;
+    if (this.stationItem && this.stationItem.temperatura) {
+      this.temperature = this.setTemperatureNumberType();
+    }
+
+    console.log(this.temperature);
   }
 
   public closeDialog(): void {
     this.dialogRef.close();
+  }
+
+  public getBackgroundColor(): string | null {
+    if (this.temperature) {
+      if (this.temperature <= 0) {
+        return '#729ABB';
+      } else if (this.temperature > 0 && this.temperature <= 13) {
+        return '#9BBAD3';
+      } else if (this.temperature > 13 && this.temperature <= 25) {
+        return '#F3D589';
+      } else if (this.temperature > 25) {
+        return '#FF8585';
+      }
+    }
+    return 'transparent';
+  }
+
+  private setTemperatureNumberType(): number {
+    return this.stationItem && this.stationItem.temperatura
+      ? +this.stationItem.temperatura
+      : 0;
   }
 }
