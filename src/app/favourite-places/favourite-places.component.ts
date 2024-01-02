cd6482a4f532e5a2f669151aea931fe2128b7a99
Dawid Drabczyk <dawid.drabczyk01@gmail.com>
2024-01-02 10:43:04 +0100

    Add deleting favourite item features at favourite component

--- src/app/favourite-places/favourite-places.component.ts
+++ src/app/favourite-places/favourite-places.component.ts
@@ -9,6 +9,7 @@ import {
   NgSwitchCase,
   UpperCasePipe,
 } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favourite-places',
@@ -20,10 +21,30 @@ import {
 export class FavouritePlacesComponent implements OnInit {
   public favouriteItems!: Array<WeatherItemDto>;
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(private weatherService: WeatherService, private router: Router) {}
 
   ngOnInit(): void {
     this.getFavouriteList();
+  }
+
+  public removeFromFavourite(weatherItem: WeatherItemDto): void {
+    const index = this.weatherService.favouriteItems.findIndex(
+      (item) => item.id === weatherItem.id
+    );
+
+    if (index !== -1) {
+      this.weatherService.favouriteItems.splice(index, 1);
+      weatherItem.isFavourite = false;
+      window.scrollTo(0, 0);
+
+      this.weatherService.favouritePlacesArray.next(
+        this.weatherService.favouriteItems
+      );
+    }
+
+    if (this.weatherService.favouriteItems.length === 0) {
+      this.router.navigate(['./']);
+    }
   }
 
   private getFavouriteList(): void {
